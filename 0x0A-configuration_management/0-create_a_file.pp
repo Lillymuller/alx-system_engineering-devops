0abacacb7cd97c210d656c150a47226f31b630d1
Lillymuller <eleltaalemu@gmail.com>
2024-03-22 07:09:56 -0700

    creat file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Using puppet, create a file with below specifications
+# Create a file in /tmp
 
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  path    => '/tmp/school',
 }
