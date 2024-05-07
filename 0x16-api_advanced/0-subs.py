78ba0464931adb603107960b3d3662420a731602
Lillymuller <eleltaalemu@gmail.com>
2024-05-07 02:45:13 -0700

    100-main.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,8 @@ def number_of_subscribers(subreddit):
     headers = {
             "User-Agent": "Mozilla/5.0"
             }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        return r.json().get("data").get("subscribers")
     else:
         return 0
